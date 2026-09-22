1f4a5399a1ba58e692071efa7ecc6ff48e759649
Smudgy0 <theohammond2006@gmail.com>
2026-09-22 23:58:40 +0100

    basic jumping added (need to add the layer collision triggers in lesson)

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -5,22 +5,31 @@ public class PlayerMovement : MonoBehaviour
 {
     // player movement values
     public float PlayerMoveSpeed;
-    public float PlayerJumpHeight;
+    public int PlayerJumpHeight;
     public bool CanJump;
+    public bool isJumping;
+    public float myHeight;
 
     // player movement direction
     [SerializeField] private Vector2 MovementDirection;
 
-    // players rigidbody
+    // players rigidbody/colliders
     private Rigidbody2D rb;
+    public BoxCollider2D MyCollider;
+    public CompositeCollider2D CC;
 
     // player character values
     public string ActiveCharater;
     private bool CanDoubleJump = false;
 
+    // Mask
+    public GameObject backGroundcollider;
+
     void Start()
     {
+        myHeight = this.transform.position.y;
         rb = GetComponent<Rigidbody2D>();
+        rb.gravityScale = 0;
     }
 
     public void OnMove(InputAction.CallbackContext value)
@@ -32,23 +41,39 @@ public class PlayerMovement : MonoBehaviour
     {
         if (value.performed)
         {
-            if (CanJump == false) { return; }
+            if (isJumping == true) { return; }
             if (rb.linearVelocityY <= -0.5f) { return; }
+            isJumping = true;
+            rb.gravityScale = 1;
             // Get player's starting jump height
+            myHeight = this.transform.position.y;
             // Make player and background layer not interact
-            rb.linearVelocityY = PlayerJumpHeight;
-            CanJump = false;
+            Physics2D.IgnoreLayerCollision(6, 7, true);
+            rb.AddForce(Vector2.up * PlayerJumpHeight, ForceMode2D.Impulse);
 
             // When landing and player has fallen with gravity if player's y value = or is close to their starting jump height
-            // Make player layer and background be able to interact
         }
     }
 
     private void FixedUpdate()
     {
-        rb.linearVelocityX = MovementDirection.x * PlayerMoveSpeed;
-
         // if(this.transform.y >)
-        rb.linearVelocityY = MovementDirection.y * PlayerMoveSpeed;
+        if (!isJumping)
+        {
+            rb.linearVelocityX = MovementDirection.x * PlayerMoveSpeed;
+            rb.linearVelocityY = MovementDirection.y * PlayerMoveSpeed;
+        }
+        //if(this.transform.position.y < myHeight && isJumping)
+        //{
+        //    rb.linearVelocityY = MovementDirection.y - rb.mass;
+        //}
+
+        if (this.transform.position.y < myHeight)
+        {
+            // Make player layer and background be able to interact
+            Physics2D.IgnoreLayerCollision(6, 7, false);
+            isJumping = false;
+            rb.gravityScale = 0;
+        }
     }
 }
